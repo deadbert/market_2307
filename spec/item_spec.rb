d2614d1c5050b18e7f6efc88146ff1f8b477f0c1
Nick Spencer <strangebrew202@gmail.com>
2023-08-08 09:05:17 -0600

    Add: base spec #initialize for Item

---
+++ spec/item_spec.rb
@@ -0,0 +1,12 @@
+require './lib/item'
+
+describe Item do
+  let(:item1) {item1 = Item.new({name: 'Peach', price: "$0.75"})}
+  let(:item1) {item2 = Item.new({name: 'Tomato', price: '$0.50'})}
+  describe "#initialize" do
+    it "has readable name and price attributes" do
+      expect(item1.name).to eq("Peach")
+      expect(item2.price).to eq(0.5)
+    end
+  end
+end 
---
+++ spec/vendor_spec.rb
@@ -0,0 +1,7 @@
+require './lib/item'
+require './lib/vendor'
+
+describe Vendor do
+  let(:item1) {item1 = Item.new({name: 'Peach', price: "$0.75"})}
+  let(:item1) {item2 = Item.new({name: 'Tomato', price: '$0.50'})}
+end
