5972cfa66e079df155490c344be1f42dee2fbb0b
Nick Spencer <strangebrew202@gmail.com>
2023-08-08 09:30:29 -0600

    Add: setup and expect tests for #add_vendor

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -5,7 +5,11 @@ require './lib/market'
 describe Market do
   let(:item1) {item1 = Item.new({name: 'Peach', price: "$0.75"})}
   let(:item2) {item2 = Item.new({name: 'Tomato', price: '$0.50'})}
-  let(:vendor) {vendor = Vendor.new("Rocky Mountain Fresh")}
+  let(:item3) {item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})}
+  let(:item4){item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})}
+  let(:vendor1) {vendor1 = Vendor.new("Rocky Mountain Fresh")}
+  let(:vendor2) {vendor2 = Vendor.new("Ba-Nom-a-Nom")}
+  let(:vendor3) {vendor3 = Vendor.new("Palisade Peach Shack")}
   let(:market) {market = Market.new("South Pearl Street Farmers Market")}
   describe "#initialize" do
     it "has readable name attribute" do
@@ -14,6 +18,21 @@ describe Market do
 
     it "initializes with empty array of vendors" do
       expect(market.vendors).to eq([])
+    end
+  end
+
+  describe "#add_vendor" do
+    it "adds a vendor object to the vendor list" do
+      vendor1.stock(item1, 35)
+      vendor1.stock(item2, 7)
+      vendor2.stock(item4, 50)
+      vendor2.stock(item3, 25)
+      vendor3.stock(item1, 65)
+      market.add_vendor(vendor1)
+      market.add_vendor(vendor2)
+      market.add_vendor(vendor3)
+
+      expect(market.vendors).to eq([vendor1, vendor2, vendor3])
     end
   end
 end
