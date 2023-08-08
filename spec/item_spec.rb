440937e621d7eee6e7bc9de048b70e148f6c5e78
Nick Spencer <strangebrew202@gmail.com>
2023-08-08 09:09:35 -0600

    Add: bare bones for Item class

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,7 +2,7 @@ require './lib/item'
 
 describe Item do
   let(:item1) {item1 = Item.new({name: 'Peach', price: "$0.75"})}
-  let(:item1) {item2 = Item.new({name: 'Tomato', price: '$0.50'})}
+  let(:item2) {item2 = Item.new({name: 'Tomato', price: '$0.50'})}
   describe "#initialize" do
     it "has readable name and price attributes" do
       expect(item1.name).to eq("Peach")
