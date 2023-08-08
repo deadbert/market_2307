370c939fe85d1e6f7a0cffbd4004854e637dcd7c
Nick Spencer <strangebrew202@gmail.com>
2023-08-08 09:12:16 -0600

    Add: #initialize spec tests for vendor

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -4,4 +4,14 @@ require './lib/vendor'
 describe Vendor do
   let(:item1) {item1 = Item.new({name: 'Peach', price: "$0.75"})}
   let(:item1) {item2 = Item.new({name: 'Tomato', price: '$0.50'})}
+  let(:vendor) {vendor = Vendor.new("Rocky Mountain Fresh")}
+  describe "#initialize" do
+    it " has readable name" do
+      expect(vendor.name).to eq("Rocky Mountain Fresh")
+    end
+
+    it "initializes with empty inventory hash" do
+      expect(vendor.inventory).to eq({})
+    end
+  end
 end
