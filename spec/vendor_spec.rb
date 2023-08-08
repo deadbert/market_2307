6d32fdd0099de33177b565e2f9fbae2188dea3a9
Nick Spencer <strangebrew202@gmail.com>
2023-08-08 09:14:40 -0600

    Add: #check_stock spec test

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -6,12 +6,18 @@ describe Vendor do
   let(:item1) {item2 = Item.new({name: 'Tomato', price: '$0.50'})}
   let(:vendor) {vendor = Vendor.new("Rocky Mountain Fresh")}
   describe "#initialize" do
-    it " has readable name" do
+    it "has readable name" do
       expect(vendor.name).to eq("Rocky Mountain Fresh")
     end
 
     it "initializes with empty inventory hash" do
       expect(vendor.inventory).to eq({})
+    end
+  end
+
+  describe "#check_stock" do
+    it "returns 0 by default if item out of stock" do
+      expect(vendor.check_stock(item1)).to eq(0)
     end
   end
 end
